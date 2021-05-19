0b937c9fe24f14834c0e125a5909d9982b6dcf91
Gregory Huras <gregory.huras@gmail.com>
2021-05-19 17:19:52 -0400

    Revert "third try"
    This reverts commit ac081b14ed7503a6670c03ebea98dd0f08427f34.

---
+++ andrew/logbook-template.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			body {
+				background-color: white;
+				margin: 20px;}
+
+			h1{
+				color: black;
+				margin-left: 60px;
+				margin-top: 40px;
+				padding-left: 20px;
+				padding-right: 40px;
+				font-family: Arial;
+				font-size: 30px;}
+
+			p{
+				color: black;
+				padding-left: 200px;
+				padding-right: 200px;
+                                font-family: Arial;
+                                font-size: 18px;}
+
+			a{
+                                color: blue;
+                                padding-left: 80px;
+				padding-right: 100px;
+                                font-family: Arial;
+                                font-size: 18px;}
+
+			ul{
+                                color: black;
+                                padding-left: 250px;
+				padding-right: 200px;
+                                font-family: Arial;
+                                font-size: 18px;}
+		</style>
+	</head>
+	<body>
+
+
+	<! This is where you input the title of your logbook entry >
+	<h1>Week 1 Logbook Entry</h1>
+
+	<! This is where you write your content for your logbook.>
+	<p>
+	This is where you write the content for your logbooks.
+	</p>
+
+	<a href="http://142.156.193.130:50000/greg/logbook.php"> Return to Main menu </a>
+
+
+
+	</body>
+</html>
+
---
+++ greg/week1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			body {
+				background-color: white;
+				margin: 20px;}
+
+			h1{
+				color: black;
+				margin-left: 60px;
+				margin-top: 40px;
+				padding-left: 20px;
+				padding-right: 40px;
+				font-family: Arial;
+				font-size: 30px;}
+
+			p{
+				color: black;
+				padding-left: 200px;
+				padding-right: 200px;
+                                font-family: Arial;
+                                font-size: 18px;}
+
+			a{
+                                color: blue;
+                                padding-left: 80px;
+				padding-right: 100px;
+                                font-family: Arial;
+                                font-size: 18px;}
+
+			ul{
+                                color: black;
+                                padding-left: 250px;
+				padding-right: 200px;
+                                font-family: Arial;
+                                font-size: 18px;}
+		</style>
+	</head>
+	<body>
+
+
+	<! This is where you input the title of your logbook entry >
+	<h1>Week 1 Logbook Entry</h1>
+
+	<! This is where you write your content for your logbook.>
+	<p>
+	This week one of the tasks I completed was I had built a framework to use the logbook on the website of the remote server.
+	The framework consists of a main menu page that holds links to all of the seperate pages to each logbook entry per week.
+	The framework I created is very barebones and contains a portion of CSS to style the entrys so everything looks better. 
+	I also created a template that both caleb and Andrew have in their posession to copy and paste so all we have to do to write content in the logbooks and dont have to do any styling. 
+	</p>
+
+	<a href="http://142.156.193.130:50000/greg/logbook.php"> Return to Main menu </a>
+
+
+
+	</body>
+</html>
+
