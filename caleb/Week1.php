6513522ab761a1cfd0325dd7af4ae9c745a9dfc3
Caleb Hoeksema <choeksema4707@conestogac.on.ca>
2021-05-20 17:13:35 -0400

    Added my Week 1 logbook

--- caleb/Week1.php
+++ caleb/Week1.php
@@ -4,58 +4,58 @@
 		<style>
 			body {
 				background-color: white;
-				margin: 20px;}
+				margin: 5%;
+				padding-left: 5%;
+				padding-right: 5%;}
 
 			h1{
 				color: black;
-				margin-left: 60px;
-				margin-top: 40px;
-				padding-left: 20px;
-				padding-right: 40px;
 				font-family: Arial;
 				font-size: 30px;}
 
 			p{
 				color: black;
-				padding-left: 200px;
-				padding-right: 200px;
                 font-family: Arial;
                 font-size: 18px;}
 
 			a{
                 color: blue;
-                padding-left: 80px;
-				padding-right: 100px;
                 font-family: Arial;
                 font-size: 18px;}
-
 			ul{
                 color: black;
-                padding-left: 250px;
-				padding-right: 200px;
                 font-family: Arial;
                 font-size: 18px;}
+			li{
+                color: black;
+                font-family: Arial;
+                font-size: 18px;}
+			img{
+                padding-left: 1%;
+				width: 300px;
+				height: 386px;}
 		</style>
 	</head>
 	<body>
-
-
-	<! This is where you input the title of your logbook entry >
-	<h1>Week 1 Logbook Entry</h1>
-
-	<! This is where you write your content for your logbook.>
-	<p>
+		
+		<h1>Week 1 Logbook Entry</h1>
+	
 		<ul>
 			<li>Set up Project Plan, dividing tasks equally and according to experience</li>
 			<li>Added the project plan to the Confluence page for review and discussed it with team-mates</li>
 			<li>Attempted STM32 HAL programming using STM32CubeMx and Atollic TrueSTUDIO but it didn't work</li>
 		</ul>
-	</p>
-
-	<a href="http://142.156.193.130:50000/caleb/logbook.php"> Return to Main menu </a>
-
-
+		
+		<p>
+			<a href="https://github.com/gr8gregory/Engineering-Projects-VI-website">Github Repository Link</a>
+			<br>
+			<br>
+			<a href="http://142.156.193.130:50000/caleb/logbook.php"> Return to Logbook Menu </a>
+		</p>
 
 	</body>
 </html>
+
+
+
 
--- caleb/logbook.php
+++ caleb/logbook.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
 <html>
-<h1> Welcome to Caleb's Logbook </h1>
-<html>
+	<head>
+		<style>
+			body {
+				background-color: white;
+				margin: 5%;
+				padding-left: 5%;
+				padding-right: 5%;}
+
+			h1{
+				color: black;
+				font-family: Arial;
+				font-size: 30px;}
+
+			p{
+				color: black;
+                font-family: Arial;
+                font-size: 18px;}
+
+			a{
+                color: blue;
+                font-family: Arial;
+                font-size: 18px;}
+			ul{
+                color: black;
+                font-family: Arial;
+                font-size: 18px;}
+		</style>
+	</head>
+	<body>
+
+
+	<h1>Welcome to Caleb's Logbook</h1>
+    <br>
+    <p><a href="http://142.156.193.130:50000/caleb/week1.php"> Week 1 </a></p>
+
+
+	<p><a href="http://142.156.193.130:50000/about.html"> Return to Main menu </a></p>
+
+	</body>
+</html>
