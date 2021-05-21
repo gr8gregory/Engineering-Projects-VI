fbf57f2e0b49a51c70659cab7be9b4ab90bf90aa
jandsam64 <asammut6065@conestogac.on.ca>
2021-05-21 10:31:25 -0400

    Created changes to my logbook for this weeks debreif

--- andrew/logbook.php
+++ andrew/logbook.php
@@ -1,3 +1,64 @@
+<!DOCTYPE html>
 <html>
-<h1> Welcome to Andrews Logbook </h1>
-<html>
+	<head>
+		<style>
+			body {
+				background-color: white;
+				margin: 20px;}
+
+			h1{
+				color: black;
+				margin-left: 60px;
+				margin-top: 40px;
+				padding-left: 20px;
+				padding-right: 40px;
+				font-family: Arial;
+				font-size: 30px;}
+
+			p{
+				color: black;
+				padding-left: 200px;
+				padding-right: 200px;
+                                font-family: Arial;
+                                font-size: 18px;}
+
+			a{
+                                color: blue;
+                                padding-left: 80px;
+				padding-right: 100px;
+                                font-family: Arial;
+                                font-size: 18px;}
+
+			ul{
+                                color: black;
+                                padding-left: 250px;
+				padding-right: 200px;
+                                font-family: Arial;
+                                font-size: 18px;}
+		</style>
+	</head>
+	<body>
+
+
+	<! This is where you input the title of your logbook entry >
+	<h1>Week 1 Logbook Entry</h1>
+
+	<! This is where you write your content for your logbook.>
+	<p>
+	Tasks:
+	-Created a confluence page to store all the teams information and methods. This helped streamline the teams development environment		so we can share our tips and tricks to work effecitvley.
+	-Wrote a guide on how to use WSL for ssh, as well as how to access the VPN from the command line and make vpn aware commands. This 		allows a user to ssh into the pi using a single command and automates the start and the stop of the vpn
+	-Worked on ensuring XAMPP worked correctly in WSL, added an information to confluence that documents my testing process for WSL, as		 well as how to update WSL to use the newest version with grpahics capability
+	-Working to create a git repo that has all development tools so it can be easily deployed by different group members, considering  		the possiblity of using containerization
+	-Starting to learn and get comfortable with CAN protocols
+	-Researching if I can use GDB with NUCLEO STMF303RE
+	-Met with group members to discuss the Gantt chart, we might make adjustments in the future to ensure division of work. 
+	</p>
+
+	<a href="http://142.156.193.130:50000/andrew/logbook.php"> Return to Main menu </a>
+
+
+
+	</body>
+</html>
+
