5ff3cc4c0128904509e5145a41d71e57bb51e701
gr8gregory <gregory.huras@gmail.com>
2021-05-28 14:27:57 -0400

    Added CAN video to video.html

--- gui.php
+++ gui.php
@@ -1,4 +1,5 @@
 <?php
+	
 	function update_elevatorNetwork(int $node_ID, int $new_floor =1): int {
 		$db1 = new PDO('mysql:host=127.0.0.1;dbname=elevator','ese','ese');
 		$query = 'UPDATE elevatorNetwork 
@@ -29,24 +30,25 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<link href='css/gui.css' type='text/css' rel='stylesheet'/> 
-	<title>Graphical User Interface</title>
+		<link href='css/gui.css' type='text/css' rel='stylesheet'/> 
+		<title>Graphical User Interface</title>
 	</head>
 
 	<body>
 	<h1>ESE Project VI Elevator GUI</h1> 
-	
 		<?php 
+			// When the "GO" button is pressed, it sends the value of the new floor the user want to the elevator network. 
+			// and it then refreshes the page and re-loads.
 			if(isset($_POST['newfloor'])) {
 				$curFlr = update_elevatorNetwork(1, $_POST['newfloor']); 
-				header('Refresh:0; url=index.php');	
+				header('Refresh:0; url=gui.php');	
 			} 
 			$curFlr = get_currentFloor();
 			echo "<h2>Current floor # $curFlr </h2>";			
 		?>		
 		
 		<h2> 	
-			<form action="index.php" method="POST">
+			<form action="gui.php" method="POST">
 				Request floor # <input type="number" style="width:50px; height:40px" name="newfloor" max=3 min=1 required />
 				<input type="submit" value="Go"/>
 				<input type="submit" value=floor1/>
