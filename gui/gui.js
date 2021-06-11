b11714cf3327f402899f363336aae8d7ac9ee467
gr8gregory <gregory.huras@gmail.com>
2021-06-11 17:14:41 -0400

    Updated greg's logbook and made gui images clickable

---
+++ gui/gui.js
@@ -0,0 +1,104 @@
+function myFunction_CD() {
+    console.log(document.getElementById("CD").src);
+    
+    if(document.getElementById("CD").src == "http://142.156.193.130:50000/gui/img/CallButtonDownLit.png"){
+        document.getElementById("CD").src="http://142.156.193.130:50000/gui/img/CallButtonDown.png";
+        console.log("true");
+    }
+    else{
+        console.log("else");
+        document.getElementById("CD").src="http://142.156.193.130:50000/gui/img/CallButtonDownLit.png";
+    }
+
+}
+function myFunction_CUD_U() {
+
+    if(document.getElementById("CUD").src == "http://142.156.193.130:50000/gui/img/call_button_lit_up.png"){
+        document.getElementById("CUD").src="http://142.156.193.130:50000/gui/img/CallButtonUpDown.png";
+    }
+    else{
+        document.getElementById("CUD").src="http://142.156.193.130:50000/gui/img/call_button_lit_up.png";
+    }
+    
+}
+function myFunction_CUD_D() {
+
+    if(document.getElementById("CUD").src == "http://142.156.193.130:50000/gui/img/call_button_lit_down.png"){
+        document.getElementById("CUD").src="http://142.156.193.130:50000/gui/img/CallButtonUpDown.png";
+    }
+    else{
+        document.getElementById("CUD").src="http://142.156.193.130:50000/gui/img/call_button_lit_down.png";
+    }
+}
+function myFunction_CU() {
+
+    if(document.getElementById("CU").src == "http://142.156.193.130:50000/gui/img/CallButtonLitUp.png"){
+        document.getElementById("CU").src="http://142.156.193.130:50000/gui/img/CallButtonUp.png";
+    }
+    else{
+        document.getElementById("CU").src="http://142.156.193.130:50000/gui/img/CallButtonLitUp.png";
+    }
+}
+function myFunction_I3() {
+
+    if(document.getElementById("INO").src == "http://142.156.193.130:50000/gui/img/Panel_3.png"){
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/none_lit_up.png";
+    }
+    else{
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/Panel_3.png";
+    }
+}
+function myFunction_I2() {
+
+    if(document.getElementById("INO").src == "http://142.156.193.130:50000/gui/img/Panel_2.png"){
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/none_lit_up.png";
+    }
+    else{
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/Panel_2.png";
+    }
+}
+function myFunction_I1() {
+
+    if(document.getElementById("INO").src == "http://142.156.193.130:50000/gui/img/Panel_1.png"){
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/none_lit_up.png";
+    }
+    else{
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/Panel_1.png";
+    }
+}
+function myFunction_IC() {
+
+    if(document.getElementById("INO").src == "http://142.156.193.130:50000/gui/img/Panel_O.png"){
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/none_lit_up.png";
+    }
+    else{
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/Panel_O.png";
+    }
+}
+function myFunction_IO() {
+
+    if(document.getElementById("INO").src == "http://142.156.193.130:50000/gui/img/Panel_C.png"){
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/none_lit_up.png";
+    }
+    else{
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/Panel_C.png";
+    }
+}
+function myFunction_IF() {
+
+    if(document.getElementById("INO").src == "http://142.156.193.130:50000/gui/img/Panel_F.png"){
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/none_lit_up.png";
+    }
+    else{
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/Panel_F.png";
+    }
+}
+function myFunction_IB() {
+
+    if(document.getElementById("INO").src == "http://142.156.193.130:50000/gui/img/Panel_B.png"){
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/none_lit_up.png";
+    }
+    else{
+        document.getElementById("INO").src="http://142.156.193.130:50000/gui/img/Panel_B.png";
+    }
+}
